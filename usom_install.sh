2450d5f71b6b0a3b8feb14097e8424ee8d0d224e
Semseddin AKSOY <semsaksoy@gmail.com>
2019-03-23 22:37:26 +0300

    fix name

--- usom_install.sh
+++ usom_install.sh
@@ -9,7 +9,7 @@ wget https://raw.githubusercontent.com/semsaksoy/qradar_usom/master/usom_update.
 
 #cron
 crontab -l > /root/USOM/cron.bck
-crontab -l | { cat; echo -e "\n# USOM list update\n5 * * * * /bin/bash /root/USOM/update.sh >/dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo -e "\n# USOM list update\n5 * * * * /bin/bash /root/USOM/usom_update.sh >/dev/null 2>&1"; } | crontab -
 
 cd /opt/qradar/bin
 ./ReferenceDataUtil.sh create usom_url_list MAP ALN
