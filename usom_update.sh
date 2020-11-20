29699c034aa84b38d628c66fc9ab970fed89c694
Şemseddin AKSOY <semsaksoy@gmail.com>
2020-11-20 14:34:25 +0300

    Update usom_update.sh

--- usom_update.sh
+++ usom_update.sh
@@ -8,4 +8,5 @@ sed -i '1 i\data' url-list.txt
 cd /opt/qradar/bin
 ./ReferenceDataUtil.sh purge usom_url_list
 ./ReferenceDataUtil.sh load usom_url_list /root/USOM/url-list.txt
-logger -T -p local3.info -n 127.0.0.1 -P 514 -i USOM "Usom updated: " . `wc -l url-list.txt`
+logger -T -p local3.info -n 127.0.0.1 -P 514 -i USOM "Usom updated: " . `wc -l /root/USOM/url-list.txt`
+
